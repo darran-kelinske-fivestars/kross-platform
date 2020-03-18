010f5ea6220b6cfe66fff54032411c78c43d33c8
Darran Kelinske <darran.kelinske@gmail.com>
2020-03-18 09:51:24 -0500

    Rename native message

--- src/commonMain/kotlin/com/fivestars/krossplatform/model/message/NativeDataMessage.kt
+++
@@ -1,3 +0,0 @@
-package com.fivestars.krossplatform.model.message
-
-class NativeDataMessage(val callback: String?, val data: String?)
---
+++ src/commonMain/kotlin/com/fivestars/krossplatform/model/message/NativeMessage.kt
@@ -0,0 +1,3 @@
+package com.fivestars.krossplatform.model.message
+
+class NativeMessage(val callback: String?, val data: String?)
